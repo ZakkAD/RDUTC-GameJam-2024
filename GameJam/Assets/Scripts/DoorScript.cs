9cc875184cb50660a765d56fa40780e508fb194a
Ty <113171957+Gigas860@users.noreply.github.com>
2024-02-06 19:59:06 +0000

    updated door script

--- GameJam/Assets/Scripts/DoorScript.cs
+++ GameJam/Assets/Scripts/DoorScript.cs
@@ -4,23 +4,23 @@ using UnityEngine;
 
 public class DoorScript : MonoBehaviour
 {
+    public GameObject door;
+    public float moveSpeed;
+    public Transform doorUp;
+    public Transform doorDown;
     public bool button;
-    public float smoothTime = 0.5f;
-    public float speed = 10;
-    Vector3 velocity;
-
-    //move object to a set position     
-    transform.position = new Vector3(Doorx, Doory+10,1);
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.SmoothDamp(Door.y+10, ref velocity, smoothTime, speed);
+        if (button == false)
+        {
+            door.transform.position = Vector3.MoveTowards(door.transform.position, doorDown.transform.position, Time.deltaTime * moveSpeed);
+        }
+
+        if (button == true)
+        {
+            door.transform.position = Vector3.MoveTowards(door.transform.position, doorUp.transform.position, Time.deltaTime * moveSpeed);
+        }
     }
 }
